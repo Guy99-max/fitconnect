c82e5b7ba156e3128303fad4f85db0dc0ab4f292
Guy99-max <ikaandsodi@gmail.com>
2025-08-02 18:34:01 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // מאפשר ל-Vercel לטעון קבצים מ-root
+  base: "/", // מאפשר ל-Vercel לטעון קבצים מ-root
   build: {
     outDir: "dist" // תיקייה סופית ל-build
   },
